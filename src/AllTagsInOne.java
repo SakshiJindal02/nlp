824783ad7c4ee22f865f6d9859f3e9d2fa7f2d33
Anmol Saxena <anmol.saxena@proptiger.com>
2018-02-21 19:15:58 +0530

    enhanced model based on updated dataset

--- src/AllTagsInOne.java
+++ src/AllTagsInOne.java
@@ -6,15 +6,106 @@ import java.util.List;
 
 public class AllTagsInOne {
 
-    private static final String BHK_FILTER = "<START:BHK> ";
-    private static final String END             = " <END> ";
-    private static final String LOCALITY_FILTER = "<START:COLUMBUS> ";
-    private static final String CITY_FILTER     = "<START:COLUMBUS> ";
+    private static final String BHK_FILTER           = "<START:BHK> ";
+    private static final String END                  = " <END> ";
+    private static final String LOCALITY_FILTER      = "<START:COLUMBUS> ";
+    private static final String CITY_FILTER          = "<START:COLUMBUS> ";
     private static final String PROPERTY_TYPE_FILTER = "<START:PROPERTY_TYPE> ";
-    private static final String FILENAME             = "/Users/user/temp/trainingDataSet.txt";
+    private static final String PRICE_FILTER         = "<START:PRICE> ";
+    private static final String FILENAME             = "/home/anmol/trainingDataSet2.txt";
+    
+    //{ "3", "BHK", "apartment", "under", "10000", "in", "Chembur Mumbai" }
+    public static String type1LocalityCityQuery(String locality, String bhk, String city, String propertyType, String price) {
+        StringBuilder text = new StringBuilder();
+        
+        addFilter(BHK_FILTER, bhk, text);
+        addFilter(PROPERTY_TYPE_FILTER, propertyType, text);
+        addFilter(PRICE_FILTER, price, text);        
+        String locality_city = locality + " " + city;
+        addFilter(LOCALITY_FILTER, locality_city, text);        
+        return text.toString();
+    }
+    
+    //{ "3", "BHK", "apartment", "under", "10000", "in", "Chembur" }
+    public static String type1CityQuery(String locality, String bhk, String city, String propertyType, String price) {
+        StringBuilder text = new StringBuilder();
+
+        addFilter(BHK_FILTER, bhk, text);
+        addFilter(PROPERTY_TYPE_FILTER, propertyType, text);
+        addFilter(PRICE_FILTER, price, text);        
+        text.append(" in ");        
+        addFilter(CITY_FILTER, city, text);
+        return text.toString();
+    }
+    
+    //{ "3", "BHK", "apartment", "under", "10000", "in", "Mumbai" }
+    public static String type1LocalityQuery(String locality, String bhk, String city, String propertyType, String price) {
+        StringBuilder text = new StringBuilder();
+        
+        addFilter(BHK_FILTER, bhk, text);
+        addFilter(PROPERTY_TYPE_FILTER, propertyType, text);
+        addFilter(PRICE_FILTER, price, text);        
+        text.append(" in ");        
+        addFilter(LOCALITY_FILTER, locality, text);
+        return text.toString();
+    }
+
+    //{ "under", "1000000/1cr", "3", "BHK", "apartment", "in", "Mumbai" }
+    public static String type2CityQuery(String locality, String bhk, String city, String propertyType, String price) {
+        StringBuilder text = new StringBuilder();
+        
+        addFilter(PRICE_FILTER, price, text);
+        addFilter(BHK_FILTER, bhk, text);
+        addFilter(PROPERTY_TYPE_FILTER, propertyType, text);        
+        text.append(" in ");        
+        addFilter(CITY_FILTER, locality, text);
+        return text.toString();
+    }
+    
+    //////////////////////////////////
+    //{ "apartment", "under", "1000000/1cr", "having", "3", "bedroom", "in", "Mumbai" }
+    public static String type3CityQuery(String locality, String bhk, String city, String propertyType, String price) {
+        StringBuilder text = new StringBuilder();
+
+        addFilter(PROPERTY_TYPE_FILTER, propertyType, text);
+        addFilter(PRICE_FILTER, price, text);        
+        text.append(" having ");
+        addFilter(BHK_FILTER, bhk, text);        
+        text.append(" in ");        
+        addFilter(CITY_FILTER, locality, text);
+        return text.toString();
+    }
+    
+    //////////////////////////////////
+    //{ "apartment", "having", "3", "bedroom", "under", "1000000/1cr", "in", "Mumbai" }
+    public static String type4CityQuery(String locality, String bhk, String city, String propertyType, String price) {
+        StringBuilder text = new StringBuilder();
+        
+        addFilter(PROPERTY_TYPE_FILTER, propertyType, text);        
+        text.append(" having ");
+        addFilter(BHK_FILTER, bhk, text);        
+        addFilter(PRICE_FILTER, price, text);
+        text.append(" in ");
+        addFilter(CITY_FILTER, locality, text);
+        return text.toString();
+    }
+    
+    //////////////////////////////////
+    //{ "apartment", "in", "Mumbai", "having", "3", "bedroom", "under", "1000000/1cr"}
+    public static String type5CityQuery(String locality, String bhk, String city, String propertyType, String price) {
+        StringBuilder text = new StringBuilder();
+
+        addFilter(PROPERTY_TYPE_FILTER, propertyType, text);
+        text.append(" in ");
+        addFilter(CITY_FILTER, locality, text);
+        text.append(" having ");
+        addFilter(BHK_FILTER, bhk, text);
+        addFilter(PRICE_FILTER, price, text);        
+        return text.toString();
+    }
 
     public static void main(String[] args) {
-        
+
         List<String> localityList = new ArrayList<>();
 
         localityList.add("Kharghar");
@@ -68,75 +159,77 @@ public class AllTagsInOne {
         localityList.add("Dahisar");
         localityList.add("Lower Parel");
         localityList.add("Badlapur West");
-        
-        
-        List<String> cityList  = new ArrayList<>();
+
+        List<String> cityList = new ArrayList<>();
         cityList.add("Mumbai");
 
         List<String> propertyTypeList = new ArrayList<>();
-        propertyTypeList.add("flat");
         propertyTypeList.add("villa");
         propertyTypeList.add("apartment");
         propertyTypeList.add("Builder Floor");
 
         List<String> bhkList = new ArrayList<>();
 
+        bhkList.add("1BHK");
         bhkList.add("1 BHK");
+        bhkList.add("2BHK");
         bhkList.add("2 BHK");
+        bhkList.add("3BHK");
         bhkList.add("3 BHK");
-        bhkList.add("3 bedroom");
-        
+
         List<Integer> buyList = new ArrayList<>();
         
+        List<String> priceList = new ArrayList<>();
+
+        priceList.add("under 1 cr");
+        priceList.add("under 1.1 cr");
+        priceList.add("under 1.2 cr");
+        priceList.add("under 1.3 cr");
+        priceList.add("under 1.4 cr");
+        priceList.add("under 1.5 cr");
+        priceList.add("under 1.6 cr");
+        priceList.add("under 1.7 cr");
+        priceList.add("under 1.8 cr");
+        priceList.add("under 1.9 cr");
+        priceList.add("under 2 cr");
+        priceList.add("under 2.1 cr");
+        priceList.add("under 2.2 cr");
+        priceList.add("under 2.3 cr");
+        priceList.add("under 2.4 cr");
+        priceList.add("under 2.5 cr");
+        priceList.add("under 2.6 cr");
+        priceList.add("under 2.7 cr");
+        priceList.add("under 2.8 cr");
+        priceList.add("under 2.9 cr");
+        priceList.add("under 3 cr");
+
         FileWriter fw = null;
         BufferedWriter bw = null;
         try {
             fw = new FileWriter(FILENAME);
 
             bw = new BufferedWriter(fw);
-        
+            
+            
+            for (String locality : localityList) {
+                for (String bhk : bhkList) {
+                    for (String city : cityList) {
+                        for (String property : propertyTypeList) {
+                            for(String price : priceList) {
+                                writeInFile(type1LocalityCityQuery(locality, bhk, city, property, price), bw);
+                                writeInFile(type1CityQuery(locality, bhk, city, property, price), bw);
+                                writeInFile(type1LocalityQuery(locality, bhk, city, property, price), bw);
+                                writeInFile(type2CityQuery(locality, bhk, city, property, price), bw);
+                                writeInFile(type3CityQuery(locality, bhk, city, property, price), bw);
+                                writeInFile(type4CityQuery(locality, bhk, city, property, price), bw);
+                                writeInFile(type5CityQuery(locality, bhk, city, property, price), bw);
+                            }
 
-        for (String locality : localityList) {
-            for (String bhk : bhkList) {
-                for (String city : cityList) {
-                    for (String property : propertyTypeList) {
 
-                    StringBuilder text = new StringBuilder();
-                    addFilter(BHK_FILTER, bhk, text);
-
-                        addFilter(PROPERTY_TYPE_FILTER, property, text);
-
-                        text.append(" in ");
-                    String locality_city = locality + " " + city;
-                    addFilter(LOCALITY_FILTER, locality_city, text);
-                    
-                    // text.append(" ");
-                    // addFilter(CITY_FILTER, city, text);
-
-                            writeInFile(text.toString(), bw);
-
-                    StringBuilder text1 = new StringBuilder();
-
-                    addFilter(BHK_FILTER, bhk, text1);
-                        addFilter(PROPERTY_TYPE_FILTER, property, text1);
-                        text1.append(" in ");
-                    text1.append(" ");
-                    addFilter(CITY_FILTER, city, text1);
-
-                            writeInFile(text1.toString(), bw);
-
-                    StringBuilder text2 = new StringBuilder();
-                    addFilter(BHK_FILTER, bhk, text2);
-
-                        addFilter(PROPERTY_TYPE_FILTER, property, text2);
-                        text2.append(" in ");
-                    addFilter(LOCALITY_FILTER, locality, text2);
-                            writeInFile(text2.toString(), bw);
-
+                        }
                     }
                 }
             }
-        }
         }
         catch (IOException e) {
             // TODO Auto-generated catch block
@@ -170,19 +263,17 @@ public class AllTagsInOne {
 
     public static void writeInFile(String content, BufferedWriter bw) {
 
-
-      
         try {
             bw.write(content);
             bw.write("\n");
 
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
 
             e.printStackTrace();
 
         }
 
-    
     }
 
 }
